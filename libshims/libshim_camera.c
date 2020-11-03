68a63e415deb616760982d736f53281d73947b10
psychicgirl2020 <anairachu@gmail.com>
2020-11-03 12:48:56 +0000

    add shim: E HAL     : dlopen failed: cannot locate symbol _ZN7android16CameraParameters13OIS_ON_SINE_XE referenced by /system/vendor/lib/hw/camera.msm8937.so...

--- libshims/libshim_camera.c
+++ libshims/libshim_camera.c
@@ -4,3 +4,4 @@ void _ZN7android16CameraParameters11OIS_ON_ZOOME() {}
 void _ZN7android16CameraParameters12OIS_ON_STILLE() {}
 void _ZN7android16CameraParameters12OIS_ON_VIDEOE() {}
 void _ZN7android16CameraParameters13OIS_CENTERINGE() {}
+void _ZN7android16CameraParameters13OIS_ON_SINE_XE() {}
