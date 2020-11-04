2b11a9b5fb4831b80cbf67641f83f94e20be423d
PsychicGirl1 <anairachu@gmail.com>
2020-11-04 01:06:35 +0000

    add shim: E HAL     : dlopen failed: cannot locate symbol _ZN7android16CameraParameters38KEY_SUPPORTED_EFFECT_PREVIEW_FPS_RANGEE referenced by /system/vendor/lib/hw/camera.msm8937.so...

--- libshims/libshim_camera.c
+++ libshims/libshim_camera.c
@@ -9,3 +9,4 @@ void _ZN7android16CameraParameters13OIS_ON_SINE_YE() {}
 void _ZN7android16CameraParameters17KEY_OIS_SUPPORTEDE() {}
 void _ZN7android16CameraParameters23KEY_SUPPORTED_OIS_MODESE() {}
 const char _ZN7android16CameraParameters26PIXEL_FORMAT_YUV420SP_NV21E[] = "nv21";
+void _ZN7android16CameraParameters38KEY_SUPPORTED_EFFECT_PREVIEW_FPS_RANGEE() {}
